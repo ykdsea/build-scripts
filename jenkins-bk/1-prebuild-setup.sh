72562601cbc35ccd98f4e54d7d8a7c1a7fb456c2
Sky Zhou <sky.zhou@amlogic.com>
2017-03-10 15:01:31 +0800

    update pre build shell.

--- jenkins-bk/1-prebuild-setup.sh
+++ jenkins-bk/1-prebuild-setup.sh
@@ -7,12 +7,15 @@
 
 UBOOT_SOURCE_PATH="$WORKSPACE/source/uboot"
 GMS_INSTALL_PATH="$WORKSPACE/source/vendor/google"
-	
-# UPDATE UBOOT FOR SECUREOS.
+
+
+##########################################################################
+#functions.
+##########################################################################
 BUILD_PRE_UPDATE_SECUREOS=FALSE
 BUILD_PRE_INSTALL_GMS=FALSE
 
-
+#check build type.
 if [ "$BUILD_TYPE" = "DRM" ]
 then
 	BUILD_PRE_UPDATE_SECUREOS=TRUE
@@ -20,12 +23,17 @@ elif [ "$BUILD_TYPE" = "ATV" ]
 then
 	BUILD_PRE_UPDATE_SECUREOS=TRUE
 	BUILD_PRE_INSTALL_GMS=TRUE
+else
+	echo "nothing to do for "$BUILD_TYPE
+	exit 0
 fi
 
 
+##########################################################################
+#check build type.
+##########################################################################
 if [ "$BUILD_PRE_UPDATE_SECUREOS" = "TRUE" ]
 then
-
 	echo "UBOOT UPDATE("$BUILD_TYPE"): update bl32.img for secureos."
 	cp -f $WORKSPACE/source/vendor/amlogic/tdk/secureos/bl32.img $UBOOT_SOURCE_PATH/fip/gxl
 	if [ $? -ne 0 ]
@@ -35,11 +43,11 @@ then
 	fi
 fi
 
-
+##########################################################################
 # INSTALL GMS PACKAGE FROM LOCAL DIRECTY.
+##########################################################################
 if [ "$BUILD_PRE_INSTALL_GMS" = "TRUE" ]
 then
-
 	$AML_SCRIPTS_PATH/android/install_gms_package.sh  $GMS_VERSION $GMS_INSTALL_PATH
 	if [ $? -ne 0 ]
 	then
