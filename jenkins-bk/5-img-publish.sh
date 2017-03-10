7214b4f581a0f9d201690a963f39a085433d5e54
Sky Zhou <sky.zhou@amlogic.com>
2017-03-10 17:14:22 +0800

    update jenkins publish shell

--- jenkins-bk/5-img-publish.sh
+++ jenkins-bk/5-img-publish.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #########################################################################
 #																		#
-#						save jenkins build url							#
+#						scp publish image								#
 #																		#
 #########################################################################
-
 
 BUILD_DATE=`date +%Y-%m-%d`
 ANDROID_BUILD_PATH="$WORKSPACE/source/out/target/product/$BOARD"
 ANDROID_BUILD_INFO=$BOARD-$BUILD_VARIANT-$BUILD_ARCH-$BUILD_TYPE
-HTTP_DEST_DIR="$FIRMWARE_UPLOAD_BASE_PATH/$BUILD_DATE/$ANDROID_BUILD_INFO-$BUILD_NUMBER"
-SCP_DEST_DIR="/data"$HTTP_DEST_DIR
+HTTP_DEST_DIR=$FIRMWARE_UPLOAD_BASE_PATH/$BUILD_DATE/$ANDROID_BUILD_INFO-$BUILD_NUMBER
+SCP_DEST_DIR=/data/$HTTP_DEST_DIR
 ANDROID_PUBLISH_SERVER=firmware-sh.amlogic.com
 
 $AML_SCRIPTS_PATH/android/scp_publish_android.sh $ANDROID_BUILD_PATH $ANDROID_PUBLISH_SERVER $SCP_DEST_DIR
@@ -22,6 +21,6 @@ fi
 ######
 #Don't remove, for description set plugin.
 ######
-echo "<Download Address>"http://$ANDROID_PUBLISH_SERVER$HTTP_DEST_DIR
+echo "<Download Address>"http://$ANDROID_PUBLISH_SERVER/$HTTP_DEST_DIR
 
 exit 0
